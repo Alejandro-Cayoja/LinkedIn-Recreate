f3991b2994878dfaa81a436b4f092f26dfc3f763
Alejandro Cayoja <Stronranger@gmail.com>
2021-05-04 08:49:21 -0300

    Configurando las opciones

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Header.css"
 import SearchIcon from '@material-ui/icons/Search';
+import HeaderOption from './HeaderOption';
 
 function Header() {
     return (
@@ -14,9 +15,12 @@ function Header() {
                 </div>
             </div>
 
-            <div className="header__right"></div>
+            <div className="header__right">
+                <HeaderOption title="Home" />
+                <HeaderOption title="My Network" />
+            </div>
         </div>
-    )
+    );
 }
 
-export default Header
+export default Header;
