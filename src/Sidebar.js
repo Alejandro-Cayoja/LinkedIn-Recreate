05ddbd64e74c439ddfa9f4a141a08478302630c4
Alejandro Cayoja <Stronranger@gmail.com>
2021-05-06 20:30:55 -0300

    Sidebar terminado con estilo

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,10 +3,18 @@ import React from 'react';
 import "./Sidebar.css";
 
 function Sidebar() {
+
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
     return (
         <div className="sidebar">
             <div className="sidebar__top">
-                <img src="" alt=""/>
+                <img src="https://image.freepik.com/vector-gratis/fondo-pantalla-formas-geometricas-neon_23-2148427188.jpg" alt=""/>
                 <Avatar className="sidebar__avatar" />
                 <h2>Alejandro Cayoja</h2>
                 <h4>Stronranger@gmail.com</h4>
@@ -25,6 +33,10 @@ function Sidebar() {
 
             <div className="sidebar__bottom">
                 <p>Recent</p>
+                {recentItem('reactjs')}
+                {recentItem('programming')}
+                {recentItem('softwareengineering')}
+                {recentItem('developer')}
             </div>
         </div>
     )
