5785e798f35222288d7e033d19ae0d300ae969d1
Alejandro Cayoja <Stronranger@gmail.com>
2021-05-07 10:35:16 -0300

    Opciones agregadas para la publicacion con estilo preparado

--- src/Feed.js
+++ src/Feed.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import "./Feed.css";
 import CreateIcon from '@material-ui/icons/Create';
+import ImageIcon from '@material-ui/icons/Image';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay';
 import InputOption from './InputOption';
 
 function Feed() {
@@ -15,7 +19,10 @@ function Feed() {
                     </form>
                 </div>
                 <div className="feed__inputOptions">
-                    <InputOption />
+                    <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9"/>
+                    <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7A33E"/>
+                    <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
+                    <InputOption Icon={CalendarViewDayIcon} title="Write article" color="#7FC15E"/>
                 </div>
             </div>
         </div>
